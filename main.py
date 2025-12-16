167be5a347c7c0e0ad8c6aa705b80f502180530c
Sakshaminsidetheinternet <sakshaminsidetheinternet@gmail.com>
2025-12-16 11:50:12 +0530

    day 2

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from fastapi import FastAPI , UploadFile, Form, File
-
+import cv2
+import numpy as np
+from analyzer.quality import quality_checker, brightness_check
 app = FastAPI(title = 'AI Social media post advisor')
 
 @app.post('/analyze')
 async def analyzer(platform : str = Form(...), file : UploadFile = File(...) ):
+    contents = await file.read()
+    np_img = np.frombuffer(contents, np.uint8)
+    cv_img = cv2.imdecode(np_img, cv2.IMREAD_COLOR)
+    is_clear , brightness_score = quality_checker(cv_img)
+    exposure = brightness_check(cv_img)
+    
     return{
-        'status' : 'ok',
         'platform' : platform,
+        'analysis' : {
+            'blur_score' : brightness_score,
+            'is_cleae' : is_clear,
+            'brightness' : exposure
+        },
         'filename' : file.filename,
 
 
